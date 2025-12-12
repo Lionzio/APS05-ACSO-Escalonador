79149619c8f84cfc20fb9ae7624a59749e39935c
viniciusarraes <viniciusarraes2024@gmail.com>
2025-12-12 12:38:38 -0300

    Criação da função geradora da tabela de processos

---
+++ src/processo.h
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+std::vector<std::vector<int>> entradaProcessos() {
+
+//tratamento da entrada
+
+int nProc;
+int quantum;
+int tTroca;
+
+char trashChar;
+
+cin >> nProc;
+cin >> trashChar;
+cin >> quantum;
+cin >> trashChar;
+cin >> tTroca;
+
+std::vector<std::vector<int>> processos;
+
+int ID;
+int tempChegada;
+int prioridade;
+int tempoCPU;
+
+for (int i = 0; i < nProc; i++) {
+    cin >> ID;
+    cin >> trashChar;
+    cin >> tempChegada;
+    cin >> trashChar;
+    cin >> prioridade;
+    cin >> trashChar;
+    cin >> tempoCPU;
+
+    processos.push_back({ID, tempChegada, prioridade, tempoCPU});
+}
+
+//o vector guarda os processos -> [1,2,3,4...,nProc] em ordem de entrada
+//dentro de cada processo há um vector que guarda ID, tempChegada...sequencialmente
+
+return processos;
+}
