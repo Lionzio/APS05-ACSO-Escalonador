de279cadb37122c936534113a2fca166c423e368
Vinicius Arraes <viniciusarraes2024@gmail.com>
2025-12-14 10:21:28 -0300

    refactor: cabeçalho de processos ajustado ao round robin

--- src/processo.h
+++ src/processo.h
@@ -3,40 +3,35 @@
 #include <iostream>
 #include <vector>
 
-
 struct Processo {
     int pid;
-    int tempo_restante;
+    int tempo_chegada;
     int prioridade;
+    int tempo_restante;
 };
 
 using namespace std;
 
-std::vector<std::vector<int>> gerarProcessos(int nProc) {
-
-std::vector<std::vector<int>> processos;
-
-int ID;
-int tempChegada;
-int prioridade;
-int tempoCPU;
+std::vector<Processo> gerarProcessos(int nProc) {
 
 char trashChar;
 
-for (int i = 0; i < nProc; i++) {
-    cin >> ID;
-    cin >> trashChar;
-    cin >> tempChegada;
-    cin >> trashChar;
-    cin >> prioridade;
-    cin >> trashChar;
-    cin >> tempoCPU;
+std::vector<Processo> processos;
 
-    processos.push_back({ID, tempChegada, prioridade, tempoCPU});
+for (int i; i < nProc; i++) {
+    Processo* temp = new Processo();
+
+    cin >> temp->pid;
+    cin >> trashChar;
+    cin >> temp->tempo_chegada;
+    cin >> trashChar;
+    cin >> temp->prioridade;
+    cin >> trashChar;
+    cin >> temp->tempo_restante;
+
+    processos.push_back(*temp);
+    delete temp;
 }
-
-//o vector guarda os processos -> [1,2,3,4...,nProc] em ordem de entrada
-//dentro de cada processo há um vector que guarda ID, tempChegada...sequencialmente
 
 return processos;
 }
