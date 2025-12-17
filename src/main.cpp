c42c2acc806e63bbe92319ad8e8d8a1f8e139288
viniciusarraes <viniciusarraes2024@gmail.com>
2025-12-17 17:01:54 -0300

    Inicialização do round robin

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,32 @@
 #include <iostream>
 #include <vector>
+
+#include "processo.h"
+#include "round_robin.cpp"
+
 using namespace std;
 
-//estrutura geral:
-//adicionar entradas gerais do simulador
-//adicionar a função gerarProcesso
-//adicionar os algoritmos
-//adicionar tratamento de saída
+int nProc;
+int quantum;
+int tTroca;
+
+char trashChar;
+
+int main() {
+    //entrada dos dados iniciais
+    std::cin >> nProc;
+    std::cin >> trashChar;
+    std::cin >> quantum;
+    std::cin >> trashChar;
+    std::cin >> tTroca;
+
+    //geração de processos
+    std::vector<Processo> processos;
+    processos = gerarProcessos(nProc);
+
+    //roun robin
+    escalonadorRoundRobin(processos, quantum, tTroca);
+
+    return 0;
+
+}
